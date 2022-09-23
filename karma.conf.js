fd37be19c01f68b581b8c51dddf1a5a23a5de9c5
imcyber-sector <71095875+imcyber-sector@users.noreply.github.com>
2022-09-23 12:21:22 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pwa-01'),
+      dir: require('path').join(__dirname, './coverage/pwa'),
       subdir: '.',
       reporters: [
         { type: 'html' },
